c0dcf70035378fb5c19e78234f4be05227a39977
LSDamnit <yuridemydko@gmail.com>
2020-07-07 20:04:21 +0400

    [Required]

--- IOS_PROJECT3/ViewModels/CreateUserViewModel.cs
+++ IOS_PROJECT3/ViewModels/CreateUserViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,16 @@ namespace IOS_PROJECT3.ViewModels
 {
     public class CreateUserViewModel
     {
+        [Required]
+        [Display(Name = "Email")]
         public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
         public string Password { get; set; }
+
+        [Required]
+        [Display(Name = "ФИО")]
         public string FIO { get; set; }
     }
 }
--- IOS_PROJECT3/ViewModels/EditUserViewModel.cs
+++ IOS_PROJECT3/ViewModels/EditUserViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,15 @@ namespace IOS_PROJECT3.ViewModels
     public class EditUserViewModel
     {
         public string Id { get; set; }
+        [Required]
+        [Display(Name = "Email")]
         public string Email { get; set; }
+     /*   [Required]
+        [Display(Name = "Пароль")]
+        [DataType(DataType.Password)]*/
         public string Password { get; set; }
+        [Required]
+        [Display(Name = "ФИО")]
         public string FIO { get; set; }
     }
 }
