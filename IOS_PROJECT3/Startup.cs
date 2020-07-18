a8d9cde9de3d881d17d1d6d8f7d94e313493ccf3
LSDamnit <yuridemydko@gmail.com>
2020-07-18 18:59:56 +0400

    +Окончательные (надеюсь) фиксы Css +Страница специальности полностью переделана

--- IOS_PROJECT3/Startup.cs
+++ IOS_PROJECT3/Startup.cs
@@ -44,29 +44,34 @@ namespace IOS_PROJECT3
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
+            
+                if (env.IsDevelopment())
+                {
                 app.UseDeveloperExceptionPage();
+               // app.UseExceptionHandler("/Home/Index");
             }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                else
+                {
+                    app.UseExceptionHandler("/Home/Index");
+                    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                    app.UseHsts();
+                }
+                app.UseHttpsRedirection();
+                app.UseStaticFiles();
+
+                app.UseRouting();
+                app.UseAuthentication();
+                app.UseAuthorization();
+
+                app.UseEndpoints(endpoints =>
+                {
+                    endpoints.MapControllerRoute(
+                        name: "default",
+                        pattern: "{controller=Home}/{action=Index}/{id?}");
+                });
             }
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-            app.UseAuthentication();
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-            });
-        }
+            
+            }
     }
-}
+
+
